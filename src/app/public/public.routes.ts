d297073b99fc023d46b7e72754399eafba151b06
113816-Tahan-Federico <88520985+Federico-Tahan@users.noreply.github.com>
2025-04-05 20:01:27 -0300

    add project screen

--- src/app/public/public.routes.ts
+++ src/app/public/public.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { LoginComponent } from './components/login/login.component';
 import { onlyPublicGuard } from '../core/shared/guards/only.public.guard';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 export const PUBLIC_ROUTES: Routes = [
     {path : '', component: LandingComponent},
+    {path : 'projects', component: ProjectsComponent},
     {path : 'login', canActivate: [onlyPublicGuard], component: LoginComponent}
 ];
