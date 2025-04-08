789a9af57db73d1a2238ac85735aeaddeeef77d5
113816-Tahan-Federico <88520985+Federico-Tahan@users.noreply.github.com>
2025-04-07 22:07:28 -0300

    add changes

---
+++ src/app/intranet/components/campaign-form/campaign-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-campaign-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './campaign-form.component.html',
+  styleUrl: './campaign-form.component.scss'
+})
+export class CampaignFormComponent {
+
+}
